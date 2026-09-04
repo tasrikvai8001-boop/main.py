890a8c18562999e450e3a684db161daf2fc7522f
tasrikvai8001-boop <tasrikvai8001@gmail.com>
2026-09-04 20:58:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def keep_alive():
 # ============================================
 # --- CONFIGURATION ---
 # ============================================
-TOKEN = "8534754260:AAFIvkNXjxuj-EdR-pdy6zSQXuvQ0Nb5b7I"  # আপনার বট টোকেন
+TOKEN = "8979865542:AAH7VUTHGERuV2y_7lDcjNRskzMind3D6h0"  # আপনার বট টোকেন
 ADMIN_ID = 7833766898          # আপনার টেলিগ্রাম ID
 BOT_NAME = "Fast Pay Bot"
 DATA_FILE = "fast_pay_bot_data.json"
